ac1c4da47319b226529c984023de384be3bc9ae1
sgvg <107176690+sgvg@users.noreply.github.com>
2022-11-28 16:13:28 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   devServer:{
       proxy:{
           '/api':{
-              target:'http://47.99.84.226:3000', //目标服务地址
+              target:'https://autumnfish.cn', //目标服务地址
               ws:'',
               changeOrigin:true, //是否开启跨域访问
               pathRewrite:{  //地址重写
